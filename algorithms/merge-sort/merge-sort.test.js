12d1b234465e39d9cde493e82b082f26ddb69beb
Tobechukwu Agbasi <tobechukwu.agbasi@alerzo.com>
2022-04-13 12:20:19 +0100

    merge sort

--- algorithms/merge-sort/merge-sort.test.js
+++ algorithms/merge-sort/merge-sort.test.js
@@ -7,12 +7,40 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  // Write your code here
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  const middle = Math.floor(nums.length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+
 };
+
+const merge = (left, right) => {
+  const results = [];
+  // let i = 0;
+  // let j = 0;
+
+  while ( left.length && right.length) {
+    if(left[0] <= right[0]){
+      results.push(left.shift());
+    }else {
+      results.push(right.shift());
+    }
+  
+}
+return results.concat(left, right);
+}
+
+
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
