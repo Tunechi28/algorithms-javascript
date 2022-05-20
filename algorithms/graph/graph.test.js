363174701307e8e62834fa5478302c47948ba00d
Tobechukwu Agbasi <tobechukwu.agbasi@alerzo.com>
2022-05-20 21:58:17 +0100

    heap sort and graph

--- algorithms/graph/graph.test.js
+++ algorithms/graph/graph.test.js
@@ -6,15 +6,13 @@
 /*
   parameters:
   myId                - number    - the id of the user who is the root node
-  
+  getUser             - function - a function that returns a user's object given an ID
   degreesOfSeparation - number   - how many degrees of separation away to look on the graph
 */
 
 /*
   getUser  - function - a function that returns a user's object given an ID
-
   example
-
   {
     id: 308,
     name: "Beatrisa Lalor",
@@ -26,12 +24,60 @@
 const { getUser } = require("./jobs");
 
 const findMostCommonTitle = (myId, degreesOfSeparation) => {
-  // code goes here
+  let queue = [myId];
+  const seen = new Set(queue);
+  const jobs = {};
+
+  for (let i = 0; i <= degreesOfSeparation; i++) {
+    const newQueue = [];
+    while (queue.length) {
+      const user = getUser(queue.shift());
+
+      // queue up next iteration of connections
+      for (let j = 0; j < user.connections.length; j++) {
+        const connection = user.connections[j];
+        if (!seen.has(connection)) {
+          newQueue.push(connection);
+          seen.add(connection);
+        }
+      }
+
+      // count the job
+      jobs[user.title] = jobs[user.title] ? jobs[user.title] + 1 : 1;
+    }
+
+    queue = newQueue;
+  }
+
+  // find key with the biggest number
+
+  const jobKeys = Object.keys(jobs);
+
+  let biggestNumber = jobs[jobKeys[0]];
+  let jobName = jobKeys[0];
+  for (let i = 1; i < jobKeys.length; i++) {
+    const currentJob = jobKeys[i];
+    if (jobs[currentJob] > biggestNumber) {
+      jobName = currentJob;
+      biggestNumber = jobs[currentJob];
+    }
+  }
+
+  // see all job titles, sorted
+  // jobKeys
+  //   .map((id) => [id, jobs[id]])
+  //   .sort((a, b) => b[1] - a[1])
+  //   .slice(0, 10)
+  //   .forEach(([job, num]) => console.log(`${num} – ${job}`));
+
+  // console.log("======");
+
+  return jobName;
 };
 
 // unit tests
 // do not modify the below code
-test.skip("findMostCommonTitle", function () {
+describe("findMostCommonTitle", function () {
   // the getUser function and data comes from this CodePen: https://codepen.io/btholt/pen/NXJGwa?editors=0010
   test("user 30 with 2 degrees of separation", () => {
     expect(findMostCommonTitle(30, 2)).toBe("Librarian");
@@ -48,7 +94,7 @@ test.skip("findMostCommonTitle", function () {
   });
 });
 
-test.skip("extra credit", function () {
+describe("extra credit", function () {
   test("user 1 with 7 degrees of separation – this will traverse every user that's followed by someone else. five users are unfollowed", () => {
     expect(findMostCommonTitle(1, 7)).toBe("Geological Engineer");
   });
